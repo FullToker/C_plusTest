bb46cd54472437a839c6d9aea7f0148bcbc859d1
FullToker <q766426243@163.com>
2023-05-11 21:45:43 +0800

    Initial Commit

---
+++ first_CplusProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  first_CplusProject
+//
+//  Created by 冯予烁 on 2023/5/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
